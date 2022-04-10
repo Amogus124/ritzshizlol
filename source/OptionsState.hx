b2f59dc008fe4d5c026ae229eaab1161e0fbf7e6
Dxgamer <95672691+Amogus124@users.noreply.github.com>
2022-04-10 18:53:51 -0300

    update OptionsState.hx

--- source/OptionsState.hx
+++ source/OptionsState.hx
@@ -30,7 +30,7 @@ using StringTools;
 // TO DO: Redo the menu creation system for not being as dumb
 class OptionsState extends MusicBeatState
 {
-	var options:Array<String> = ['Notes', 'Controls', 'Preferences'];
+	var options:Array<String> = ['Notes' #if android , 'Android Controls' #end, 'Controls', 'Preferences'];
 	private var grpOptions:FlxTypedGroup<Alphabet>;
 	private static var curSelected:Int = 0;
 	public static var menuBG:FlxSprite;
@@ -59,6 +59,10 @@ class OptionsState extends MusicBeatState
 			grpOptions.add(optionText);
 		}
 		changeSelection();
+
+    #if android
+	  addVirtualPad(UP_DOWN, A_B);
+    #end
 
 		super.create();
 	}
@@ -98,6 +102,10 @@ class OptionsState extends MusicBeatState
 
 				case 'Preferences':
 					openSubState(new PreferencesSubstate());
+        case 'Android Controls':
+          #if android
+			  	MusicBeatState.switchState(new android.AndroidControlsMenu());
+                                #end
 			}
 		}
 	}
